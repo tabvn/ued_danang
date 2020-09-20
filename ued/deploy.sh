c53077864e88f96e35bac940c3b17c0415420ef4
Toan <toan@tabvn.com>
2020-09-20 16:24:59 +0700

    update

--- ued/deploy.sh
+++ ued/deploy.sh
@@ -4,7 +4,7 @@ PROJECT="danang-288409"
 GOOS=linux GOARCH=amd64 go build -v -o ./docker/ued
 
 #gcloud beta compute ssh --zone "asia-southeast1-a" "instance-2" --project "danang-288409"
-gcloud beta compute ssh --zone "asia-southeast1-a" "$INSTANCE" --project "$PROJECT" -- 'sudo mkdir /var/www/html'
+gcloud beta compute ssh --zone "asia-southeast1-a" "$INSTANCE" --project "$PROJECT" -- 'sudo mkdir -p /var/www/html'
 gcloud compute copy-files ./docker/* root@ued:/var/www --zone "asia-southeast1-a" --project "danang-288409"
 gcloud beta compute ssh --zone "asia-southeast1-a" "$INSTANCE" --project "$PROJECT" -- 'cd /var/www; sudo docker-compose build && sudo docker-compose up -d'
 rm -r ./docker/ued
