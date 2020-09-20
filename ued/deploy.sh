ffc98942cc568bee25775880942302adf74ee35a
Toan <toan@tabvn.com>
2020-09-20 13:51:03 +0700

    deploy

--- ued/deploy.sh
+++ ued/deploy.sh
@@ -1,7 +1,10 @@
 IP='35.197.153.176'
+INSTANCE="ued"
+PROJECT="danang-288409"
 GOOS=linux GOARCH=amd64 go build -v -o ./docker/ued
 
 #gcloud beta compute ssh --zone "asia-southeast1-a" "instance-2" --project "danang-288409"
-gcloud compute copy-files ./docker/* root@instance-2:/var/www/ued.tabvn.com/api --zone "asia-southeast1-a" --project "danang-288409"
-gcloud beta compute ssh --zone "asia-southeast1-a" "instance-2" --project "danang-288409" -- 'cd /var/www/ued.tabvn.com/api; sudo docker-compose build && sudo docker-compose up -d'
+gcloud beta compute ssh --zone "asia-southeast1-a" "$INSTANCE" --project "$PROJECT" -- 'sudo mkdir /var/www/html'
+gcloud compute copy-files ./docker/* root@ued:/var/www --zone "asia-southeast1-a" --project "danang-288409"
+gcloud beta compute ssh --zone "asia-southeast1-a" "$INSTANCE" --project "$PROJECT" -- 'cd /var/www; sudo docker-compose build && sudo docker-compose up -d'
 rm -r ./docker/ued
